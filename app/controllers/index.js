8917903dbcec5cc14acae096a83879d0f94b4e1a
Jorge Macías García <jormagar@trantor.cc.upv.es>
2017-05-30 10:09:45 +0200

    Test Case

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,99 +1,100 @@
-['simplePost', 'basicAuthPost'].forEach(function (proxy) {
-  $.addListener($[proxy], 'click', prepareRequest);
-});
+(function controller(args) {
+  'use strict';
 
-$.index.open();
+  //Set events
+  ['simplePost', 'basicAuthPost'].forEach(function (proxy) {
+    $.addListener($[proxy], 'click', prepareRequest);
+  });
 
-function prepareRequest(e) {
-  var withCredentials = true;
+  /**
+   * @method  prepareRequest
+   * @param   {object}       e Callback event
+   */
+  function prepareRequest(e) {
+    var withCredentials = true;
 
-  if (e.source.id === 'simplePost') {
-    withCredentials = false;
-  }
-
-  console.log('WITH CREDENTIALS : ' + withCredentials);
-
-  request(withCredentials);
-}
-
-function request(withCredentials) {
-  var client,
-    config,
-    url,
-    data,
-    upv,
-    expire;
-
-  upv = true;
-  expire = false;
-
-  if (upv) {
-    url = 'xxx';
-
-    if (expire) {
-      url = 'xxx';
+    if (e.source.id === 'simplePost') {
+      withCredentials = false;
     }
 
+    request(withCredentials);
+  }
+
+  /**
+   * Make a request
+   * @method  request
+   * @param   {boolean} withCredentials Flag for make a auth request or not
+   */
+  function request(withCredentials) {
+    var client,
+      config,
+      url,
+      data;
+
     data = {
-      'grant_type': 'refresh_token',
-      'UPV_UUID': 'xxx',
-      'refresh_token': 'xxx'
+      message: 'This is my message'
     };
-  } else {
-    url = 'https://httpbin.org/post';
 
-    data = {
-      message: 'This is my message',
-      from: 'Jorge'
+    config = {
+      onload: success,
+      onerror: error,
+      timeout: 5000
     };
+
+    if (withCredentials) {
+      url = 'https://srvasic.upv.es/recursos/error401.asp';
+      config.withCredentials = true;
+      config.username = 'user';
+      config.password = 'p$wd';
+    } else  {
+      url = 'https://httpbin.org/post';
+    }
+
+    console.log('HTTP CLIENT CONFIG ' + JSON.stringify(config));
+
+    client = Ti.Network.createHTTPClient(config);
+
+    try {
+      console.log('URL: ' + url);
+      client.open('POST', url);
+    } catch (e) {
+      console.log('error opening client');
+      console.log(JSON.stringify(e));
+    }
+
+    try {
+      client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    } catch (e) {
+      console.log('error setting request headers');
+      console.log(JSON.stringify(e));
+    }
+
+    try {
+      client.send(data);
+    } catch (e) {
+      console.log('error sending data');
+      console.log(JSON.stringify(e));
+    }
   }
 
-  config = {
-    onload: success,
-    onerror: error,
-    timeout: 5000
-  };
-
-  if (withCredentials) {
-    config.withCredentials = true;
-    config.username = 'xxxx';
-    config.password = 'xxxx';
+  /**
+   * HttpClient Success Callback
+   * @method  success
+   * @param   {object} e Callback Event
+   */
+  function success(e) {
+    console.log('Success callback: ' + this.responseText);
   }
 
-  console.log('HTTP CLIENT CONFIG ' + JSON.stringify(config));
-
-  client = Ti.Network.createHTTPClient(config);
-
-  //client.abort();
-
-  try {
-    console.log('URL ' + url);
-
-    client.open('POST', url);
-  } catch (e) {
-    alert('error opening client');
-    console.log(JSON.stringify(e));
+  /**
+   * HttpClient Error Callback
+   * @method  error
+   * @param   {object} e Callback Event
+   */
+  function error(e) {
+    console.log('Error callback : ' + e.error);
   }
 
-  try {
-    client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  } catch (e) {
-    alert('error setting request headers');
-    console.log(JSON.stringify(e));
-  }
+  $.index.open();
 
-  try {
-    client.send(data);
-  } catch (e) {
-    alert('error sending data');
-    console.log(JSON.stringify(e));
-  }
-}
-
-function success(e) {
-  Ti.API.info('Received text: ' + this.responseText);
-}
-
-function error(e) {
-  Ti.API.debug(e.error);
-}
+})(arguments[0] || {});
